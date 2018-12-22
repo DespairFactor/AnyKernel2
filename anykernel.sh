e37b53a76e3399dc2e523772d0172e5f064a1b89
DespairFactor <matthewalex@outlook.com>
2018-12-22 15:42:27 -0500

    bluecross

--- anykernel.sh
+++ anykernel.sh
@@ -4,8 +4,8 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=Despair Kernel
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
@@ -17,8 +17,8 @@ device.name5=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -37,23 +37,50 @@ chown -R root:root $ramdisk/*;
 ## AnyKernel install
 dump_boot;
 
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.lz4
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=lz4 $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
+fi;
+
+
+# Patch dtbo.img on custom ROMs
+username="$(file_getprop /system/build.prop "ro.build.user")";
+echo "Found user: $username";
+case "$username" in
+  "android-build") user=google;;
+  *) user=custom;;
+esac;
+hostname="$(file_getprop /system/build.prop "ro.build.host")";
+echo "Found host: $hostname";
+case "$hostname" in
+  *corp.google.com|abfarm*) host=google;;
+  *) host=custom;;
+esac;
+if [ "$user" == "custom" -o "$host" == "custom" ]; then
+  if [ ! -z /tmp/anykernel/dtbo.img ]; then
+    ui_print " "; ui_print "You are on a custom ROM, patching kernel to remove verity...";
+    $bin/magiskboot --dtb-patch /tmp/anykernel/dtbo.img;
+    $bin/magiskboot --dtb-patch /tmp/anykernel/Image.lz4-dtb;
+    
+  fi;
+else
+  ui_print " "; ui_print "You are on stock, not patching kernel to remove verity!";
+fi;
+
+
+
 # begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
